cbae67cc49639c71dcf7716353e9836d4864dfdd
russlandy <ruslandabysov@gmail.com>
2024-01-27 18:22:51 +0300

    add styles for profile

--- components/pages/Profile.jsx
+++ components/pages/Profile.jsx
@@ -1,29 +1,29 @@
 const React = require("react");
-const Layout = require("../Layout")
-const Table = require("../Table")
+const Layout = require("../Layout");
+const Table = require("../Table");
 
-module.exports = function Profile({user, topics}) {
-    return( 
+module.exports = function Profile({ user, topics }) {
+  return (
     <Layout title={`страница пользователя: ${user.login}`}>
-        <div className='card-header'>
-        <h1>Flashcards</h1>
-        <div className='card-nav-bar'>
-          <a href='/username'>{`${user.login}`}</a>
-          <a href='/logout'>logout</a>
-          <a href='/'>home</a>
+      <div className="wrapper_header">
+        <div className="header">
+          <h1>Flashcards</h1>
+          <div className="card-nav-bar">
+            <a href="/username">{`${user.login}`}</a>
+            <a href="/logout">logout</a>
+            <a href="/">home</a>
+          </div>
         </div>
       </div>
-        <div className="wrap">
+      <div className="profile_wrap">
         <h1>{`${user.login}`}</h1>
 
-        <div>{topics.map((topic)=> (
-            <Table key={topic.id} topic={topic} user = {user} />
-        ))}</div>
-
-
-
+        <div>
+          {topics.map((topic) => (
+            <Table key={topic.id} topic={topic} user={user} />
+          ))}
         </div>
-    
+      </div>
     </Layout>
-    )
-}
+  );
+};
